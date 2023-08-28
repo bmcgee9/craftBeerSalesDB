d161aaefbbe574354f4f5c78f0ee12faa75f7837
bmcgee9 <102620872+bmcgee9@users.noreply.github.com>
2023-08-28 18:45:46 -0400

    Update countryLookup.php

--- html_CSS_PHP_Files/countryLookup.php
+++ html_CSS_PHP_Files/countryLookup.php
@@ -1,13 +1,16 @@
 <?php
+//importing style sheet for table output
 echo '<link rel="stylesheet" href="tableStyle.css">';
+//creates variables to use in remote RDS login
 $username="brett";
 $password="12345";
 $database="craftbeerdb";
 
+//collecting the information from the POST request
 $name=$_POST['Country_of_Origin'];
 //$name2 = "%" . $name . "%";
 
-
+//creates connection with RDS and executes query
 $mysqli=new mysqli("craftbeerrds3.c272vphjyl8a.us-east-1.rds.amazonaws.com",$username,$password,$database);
 $query="SELECT * from vendors where Country_of_Origin=?";
 $stmt=$mysqli->prepare($query);
@@ -16,13 +19,15 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 //$result=$mysqli->query($query) or die($mysqli->error.__LINE__);
-
+//checks to see if the query returned anything
 if ($result->num_rows > 0) {
+	//creates the structure of the html table
 	echo '<table>
 	<tr>
 		<th>Vendor</th>
 		<th>Country of Origin</th>
 	</tr>';
+	//goes through each row of results and outputs it into the table
 	while($row=$result->fetch_assoc()) {
 	//echo $row['name']."'s vendor is ".$row['Vendor_code']."</br>";
 		echo '
@@ -35,6 +40,7 @@ if ($result->num_rows > 0) {
 	</table>';
 }
 else {
+	//prints no results if there are none
 	echo 'NO RESULTS';
 }
 ?>
