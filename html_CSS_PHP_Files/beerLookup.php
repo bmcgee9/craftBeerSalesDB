3bcb0ad603a2ea06d2697d02e6dbd38bb9d87e1a
bmcgee9 <102620872+bmcgee9@users.noreply.github.com>
2023-08-28 18:43:20 -0400

    Update beerLookup.php

--- html_CSS_PHP_Files/beerLookup.php
+++ html_CSS_PHP_Files/beerLookup.php
@@ -1,13 +1,15 @@
 <?php
 echo '<link rel="stylesheet" href="tableStyle.css">';
+//setting variable values to be able to login to my RDS remotely
 $username="brett";
 $password="12345";
 $database="craftbeerdb";
 
+//collecting the information from the POST request
 $name=$_POST['name'];
 $name2 = "%" . $name . "%";
 
-
+//creating the connection to RDS and executing query with a prepared statement
 $mysqli=new mysqli("craftbeerrds3.c272vphjyl8a.us-east-1.rds.amazonaws.com",$username,$password,$database);
 $query="SELECT * from products where name LIKE ?";
 $stmt=$mysqli->prepare($query);
@@ -16,8 +18,9 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 //$result=$mysqli->query($query) or die($mysqli->error.__LINE__);
-
+//outputting the results as long as they exist
 if ($result->num_rows > 0) {
+	//creates the html structure of a table
 	echo '<table>
 	<tr>
 		<th>Name</th>
@@ -25,6 +28,7 @@ if ($result->num_rows > 0) {
 		<th>ABV</th>
 		<th>Price</th>
 	</tr>';
+	//go through each result and add its information to the table
 	while($row=$result->fetch_assoc()) {
 	//echo $row['name']."'s vendor is ".$row['Vendor_code']."</br>";
 		echo '
@@ -39,6 +43,7 @@ if ($result->num_rows > 0) {
 	</table>';
 }
 else {
+	//print no results if there are none
 	echo 'NO RESULTS';
 }
 ?>
